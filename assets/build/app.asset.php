787eb598e58adf7126d27de5b3757f105ddb3b7e
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2025-12-19 16:01:25 +0600

    added topbar

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => '61f69be3c11fd929f8e3');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => '6418abb786b74dc99a50');
--- dev_zaplane/containers/BackendDashboard/pages/workflows/workFlowMotion/CustomNode.js
+++ dev_zaplane/containers/BackendDashboard/pages/workflows/workFlowMotion/CustomNode.js
@@ -1,27 +1,60 @@
-import { Handle, Position } from "@xyflow/react";
+import { useState } from "react";
+import { Handle, Position, NodeToolbar } from "@xyflow/react";
+import { Box, Text, HStack, Icon } from "@chakra-ui/react";
+import { RiDeleteBin7Line } from "react-icons/ri";
+import { FaRegCopy } from "react-icons/fa";
 
 export default function CustomNode({ data }) {
+  const [hovered, setHovered] = useState(false);
+
   return (
-    <div
-      style={{
-        padding: 10,
-        border: "1px solid #555",
-        borderRadius: 4,
-        background: "#fff",
-        minWidth: 80,
-        textAlign: "center",
-        width: "auto"
-      }}
+    <Box
+      position="relative"
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
     >
-      <Handle
-        type="target"
-        position={Position.Left}
-      />
-      {data.label}
-      <Handle
-        type="source"
-        position={Position.Right}
-      />
-    </div>
+      <NodeToolbar
+        isVisible={hovered}
+        position={Position.Bottom}
+        align="end"
+        offset={-3}
+        
+      >
+        <HStack
+          bg="gray.800"
+          color="white"
+          px={3}
+          py={1}
+          borderRadius="md"
+          boxShadow="md"
+          cursor="pointer"
+          pointerEvents="auto"
+          onMouseEnter={() => setHovered(true)}   
+          onMouseLeave={() => setHovered(false)}
+        >
+          <Icon as={RiDeleteBin7Line} boxSize={4} />
+          <Icon as={FaRegCopy} boxSize={4} />
+        </HStack>
+      </NodeToolbar>
+      <Box
+        bg="white"
+        border="1px solid"
+        borderColor="gray.300"
+        borderRadius="md"
+        px={4}
+        py={2}
+        minW="100px"
+        textAlign="center"
+        boxShadow="sm"
+      >
+        <Handle type="target" position={Position.Left} />
+
+        <Text m={0} fontSize="sm" fontWeight="medium">
+          {data.label}
+        </Text>
+
+        <Handle type="source" position={Position.Right} />
+      </Box>
+    </Box>
   );
 }
