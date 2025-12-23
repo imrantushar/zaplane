a7d495576cddf2fee43b251c00e78a775fed3f08
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2025-12-23 11:16:27 +0600

    commnet fulfile

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7b36c44608cd16a2fda9');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e27e7edd6137fdf5e696');
--- assets/build/app.js
+++ assets/build/app.js
@@ -2753,6 +2753,7 @@ __webpack_require__.r(__webpack_exports__);
 const namespace = 'zaplane/v1/';
 const createWorkflows = (0,_reduxjs_toolkit__WEBPACK_IMPORTED_MODULE_0__.createAsyncThunk)('zaplane/createWorkflows', async (payload, thunkAPI) => {
   return await _ZAPUtils_helper__WEBPACK_IMPORTED_MODULE_1__.API.post(namespace + 'workflows', payload).then(res => {
+    console.log(res, 'response');
     (0,_ZAPUtils_helper__WEBPACK_IMPORTED_MODULE_1__.handleSliceSuccess)(thunkAPI, "data fetched successfully");
     return res.data;
   }).catch(err => {
@@ -2786,10 +2787,9 @@ const workflowsSlice = (0,_reduxjs_toolkit__WEBPACK_IMPORTED_MODULE_0__.createSl
     builder.addCase(createWorkflows.fulfilled, (state, action) => {
       state.data = [action.payload, ...state.data];
     }).addCase(getWorkFlow.fulfilled, (state, action) => {
-      const {
-        data
-      } = action.payload;
-      state.data = data;
+      // console.log(action,'action');
+      // const { data } =action.payload;
+      // state.data = data;
     });
   }
 });
