3ac2a5624e8c3e8dad198a3b1957a837c7150140
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-01-03 10:41:15 +0600

    update logs

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b947f515563198e27fe8');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b197af66a607a4d8200a');
--- dev_zaplane/containers/BackendDashboard/pages/logs/index.js
+++ dev_zaplane/containers/BackendDashboard/pages/logs/index.js
@@ -9,7 +9,8 @@ import {
 import { useDispatch, useSelector } from "react-redux";
 
 import LogDetails from "./LogDetails/LogDetails";
-import { getRunsList, retryNodeRun, } from "@ZAPRedux/Slices/logsSlice/LogsSlice";
+import { getRunsList, retryNodeRun } from "@ZAPRedux/Slices/logsSlice/logsSlice";
+
 
 
 
@@ -18,7 +19,7 @@ const Logs = () => {
     const [expandedRowId, setExpandedRowId] = useState(null);
 
     const runs = useSelector((state) => state.logs?.data || []);
-    console.log(runs);
+    console.log(runs,'runs');
 
 
     useEffect(() => {
@@ -62,7 +63,7 @@ const Logs = () => {
                         </Table.Cell>
                     </Table.Row>
                 ) : (
-                    runs.map((row) => (
+                    runs?.runs?.map((row) => (
                         <Table.Row key={row.id}>
                             <Table.Cell>
                                 <Text fontSize="sm">
