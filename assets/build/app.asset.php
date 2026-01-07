5997ad1cd4cd0cf87e679493f12d01fdcc052740
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-01-07 16:55:09 +0600

    build push

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '78166a25e6ec00717ae7');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '76db79a9a555feef482c');
