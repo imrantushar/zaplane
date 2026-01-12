7d1d9760bad90cc40986693a7b381bddc8c9af82
mizanurrahman70 <mizanurrahmanofficial70@gmail.com>
2026-01-12 08:34:05 +0200

    solved full screen hide and show issue when full scrren show then content showing parfactly  when smalll then

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b6f9ba4371261c047f2f');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a1af9dce6e3eb7269d6e');
