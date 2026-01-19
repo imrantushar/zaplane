53fe41bda4d2eb2f7d18b66c91c2ebff88881bee
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-01-19 19:15:17 +0600

    over flow issue solved

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '89478d22c25076077f01');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c032b76f4bf3677ba6f7');
