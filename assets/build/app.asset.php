1b668ef0fd11dae5a4666d6582ebb0d021a5c54f
MD Abu Abdullah <abdullah72308@gmail.com>
2026-01-20 09:33:42 +0600

    build push

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c4eb5a2427c33da8e7a8');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '31bfea9ea719eb1b0a9f');
