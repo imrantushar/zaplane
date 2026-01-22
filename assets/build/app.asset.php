1b902137b80ec859b6a59de0819f144e4d119a4a
MD Abu Abdullah <abdullah72308@gmail.com>
2026-01-22 18:03:45 +0600

    hook added to workflow api payload

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '081f82da58ecc45ca7d5');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9047f2b13a66226f9204');
