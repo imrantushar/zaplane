180a561ea9c32b43c819e000ce70d00fa33ea972
MD Abu Abdullah <abdullah72308@gmail.com>
2026-01-25 10:43:22 +0600

    color variable updated

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '14dbadf32ef2e4d7c2ab');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e8c615e6ed6af601961a');
