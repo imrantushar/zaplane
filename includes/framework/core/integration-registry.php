07fb7278afadcdb86b0ba6b691353593ec5bfd33
Md Rofiqul  Islam Rimon <mdrofiqulislam01516@gmail.com>
2026-01-28 15:40:35 +0600

    ablocks trigger add

--- includes/framework/core/integration-registry.php
+++ includes/framework/core/integration-registry.php
@@ -15,4 +15,5 @@ return [
     'variable' => ['file' => 'variable.php', 'class' => \Zaplane\Integrations\Variable::class],
     'webhook' => ['file' => 'webhook.php', 'class' => \Zaplane\Integrations\Webhook::class],
     'storeengine' => ['file' => 'storeengine.php', 'class' => \Zaplane\Integrations\Storeengine::class],
+    'ablocks' => ['file' => 'ablocks.php', 'class' => \Zaplane\Integrations\ABlocks::class],
 ];
