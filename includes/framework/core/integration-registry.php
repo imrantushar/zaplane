a6cff0357caf218046a9a9c29106d9f553a7c076
Md Rofiqul  Islam Rimon <mdrofiqulislam01516@gmail.com>
2026-02-09 12:36:20 +0600

    trigger

--- includes/framework/core/integration-registry.php
+++ includes/framework/core/integration-registry.php
@@ -15,4 +15,5 @@ return [
     'variable' => ['file' => 'variable.php', 'class' => \Zaplane\Integrations\Variable::class],
     'webhook' => ['file' => 'webhook.php', 'class' => \Zaplane\Integrations\Webhook::class],
     'storeengine' => ['file' => 'storeengine.php', 'class' => \Zaplane\Integrations\Storeengine::class],
+    'groundhogg' => ['file' => 'groundhogg.php', 'class' => \Zaplane\Integrations\Groundhogg::class],
 ];
