1108b7622109cfd248ebe3a9ba9ac54702145be6
MD Abu Abdullah <abdullah72308@gmail.com>
2026-02-15 17:07:10 +0600

    hook added into workflow update payload

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0f3ef4c6bfa5f69c8504');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0b83e2a9677981af4a95');
--- dev_zaplane/components/ZAPSelect/index.js
+++ dev_zaplane/components/ZAPSelect/index.js
@@ -1,5 +1,5 @@
 
-import { Box ,Text} from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import { __, sprintf } from "@wordpress/i18n";
 import Select from "react-select";
 
@@ -18,8 +18,8 @@ const ZAPSelect = ({
   return (
     <Box mb={mb} style={containerStyle}>
       {label && <Text className="zaplane-label" fontWeight="600" fontSize="0.875rem" mb="4px">
-         {sprintf(__('%s', 'zaplane'), label)}
-        </Text>}
+        {sprintf(__('%s', 'zaplane'), label)}
+      </Text>}
       <Select
         className="zaplane-select"
         classNamePrefix="zaplane-select"
@@ -29,7 +29,7 @@ const ZAPSelect = ({
         isClearable={isClearable}
         value={options.find(o => o.value === value) || null}
         onMenuOpen={onMenuOpen}
-        onChange={(opt) => onChange?.(opt?.value)}
+        onChange={(opt) => onChange?.(opt)}
       />
     </Box>
   );
