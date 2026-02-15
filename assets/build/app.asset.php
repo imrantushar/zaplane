cdf0de1aad0b75283184e53c142fa51eedc700d7
MD Abu Abdullah <abdullah72308@gmail.com>
2026-02-15 17:10:17 +0600

    production build

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0b83e2a9677981af4a95');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b0dcec0242e16f34d74f');
