9ff2d006f7541c56bf525a0a63439759974d856c
MD Mostofa Kamal Mostak <mostofa.bd7997@gmail.com>
2026-02-16 13:05:02 +0600

    support divi form builder

--- includes/framework/core/integration-registry.php
+++ includes/framework/core/integration-registry.php
@@ -15,4 +15,6 @@ return [
     'variable' => ['file' => 'variable.php', 'class' => \Zaplane\Integrations\Variable::class],
     'webhook' => ['file' => 'webhook.php', 'class' => \Zaplane\Integrations\Webhook::class],
     'storeengine' => ['file' => 'storeengine.php', 'class' => \Zaplane\Integrations\Storeengine::class],
+    'diviformbuilder' => ['file' => 'diviformbuilder.php', 'class' => \Zaplane\Integrations\Diviformbuilder::class],
+
 ];
---
+++ integrations/diviformbuilder.php
@@ -0,0 +1,48 @@
+<?php
+namespace Zaplane\Integrations;
+
+use Zaplane\Framework\Classes\IntegrationBase;
+
+if (!defined('ABSPATH')) exit;
+
+class Diviformbuilder extends IntegrationBase {
+
+    public static function get_slug(): string {
+        return 'diviformbuilder';
+    }
+
+    public static function get_triggers(): array {
+        return [
+       
+            'df_after_process' => ['label' => 'Form Submission', 'hook' => 'df_after_process'],
+        ];
+    }
+
+    public static function get_actions(): array {
+        return [];
+    }
+
+    public static function resolve_trigger(array $node, array $args) {
+        switch ($node['event']) {
+            case 'df_after_process':
+                 ray($args);
+                $form_data = $args[0] ?? [];
+                    ray($form_data);
+
+                return [
+                    'form_id' => $form_data['form_id'] ?? '',
+                    'post_id' => $form_data['post_id'] ?? 0,
+                    'email' => $form_data['email'] ?? '',
+                    'name' => $form_data['name'] ?? '',
+                    'message' => $form_data['message'] ?? '',
+                    'submitted_at' => current_time('mysql'),
+                ];
+        }
+
+        return false;
+    }
+
+    public static function execute_node(array $node, array $input): array {
+        return ['port' => 'main', 'data' => $input];
+    }
+}
