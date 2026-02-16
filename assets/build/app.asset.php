1506d12f6e1ea87640239d90a0b669bfc6fa8eb3
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-02-16 10:33:01 +0600

    solved hook issues

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0b83e2a9677981af4a95');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b0c703610dfae14f8b68');
