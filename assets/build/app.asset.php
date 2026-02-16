f72714313d76712794d12f4abfa6bf51f7585b8d
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-02-16 11:07:36 +0600

    Revert "solved seleted issues"
    This reverts commit 1cc4cf3748cc2bf6b4b78c636096eddeafe23d3d.

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f5f725853938b277c062');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '40acb98529464900c941');
