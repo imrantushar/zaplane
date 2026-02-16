7f8bce74bd855792fc6e80ba961de1d38ac17492
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-02-16 11:14:45 +0600

    solved selted isues

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '40acb98529464900c941');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'fa23233260a971968289');
