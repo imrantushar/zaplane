8bfc12f668561535b3c81953ca699a63d6ecbb14
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-02-16 13:29:18 +0600

    solved marge releted issues

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a27168fddddcdceb850e');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '828189323d4d19a3a305');
