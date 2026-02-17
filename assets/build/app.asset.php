5b5968c9f0790571a31666cd4d4b13db2f03957b
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-02-17 21:55:52 +0600

    worflow option stasus update

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '454d50d8ffb4f98ac773');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8fabc2058b2c8a929fc1');
--- dev_zaplane/containers/BackendDashboard/pages/workflows/WorkflowTable.js
+++ dev_zaplane/containers/BackendDashboard/pages/workflows/WorkflowTable.js
@@ -16,6 +16,7 @@ import {
   deleteWorkFlow,
   updateWorkFlowStatus,
 } from "@ZAPRedux/Slices/workFlowSlice/actions/workFlow";
+import StatusOptions from "@ZAPComponents/StatusOptions";
 
 const WorkflowTable = () => {
   const navigate = useNavigate();
@@ -25,148 +26,120 @@ const WorkflowTable = () => {
     (state) => state.workflows
   );
 
-  const handleDelete = useCallback(
-    (id) => {
-      if (
-        window.confirm(
-          __("Are you sure you want to permanently delete ?", "zaplane")
+  const columns = [
+    {
+      name: (
+        <Text className="zaplane-label">
+          {__("Title", "zaplane")}
+        </Text>
+      ),
+      cell: (row) => (
+        <Text
+          className="zaplane-label"
+          fontWeight="500"
+          cursor="pointer"
+          onClick={() =>
+            navigate(
+              `${route_path}admin.php?page=zaplane-workflows&action=edit&id=${row.id}`
+            )
+          }
+        >
+          {row.title}
+        </Text>
+      ),
+      columnWidth: "100px",
+      textAlign: 'start'
+    },
+    {
+      name: (
+        <Text className="zaplane-label">
+          {__("Created At", "zaplane")}
+        </Text>
+      ),
+      cell: (row) => (
+        <Text fontSize="sm">{row.created_at}</Text>
+      ),
+      columnWidth: "160px",
+      textAlign: "center",
+    },
+    {
+      name: (
+        <Text className="zaplane-label">
+          {__("Updated At", "zaplane")}
+        </Text>
+      ),
+      cell: (row) => (
+        <Text className="zaplane-label">{row.updated_at}</Text>
+      ),
+      columnWidth: "160px",
+    },
+    {
+      name: (
+        <Text className="zaplane-label">
+          {__("Status", "zaplane")}
+        </Text>
+      ),
+      cell: (row) => {
+        const handleStatusChange = (row, newStatus) => {
+          if (!row?.id || !newStatus) return;
+          dispatch(updateWorkFlowStatus({ id: row.id, status: newStatus }));
+        };
+
+        return (
+          <StatusOptions
+            value={row?.status}
+            options={{
+              items: [...statusOptions],
+            }}
+            onChangeHandler={(newStatus) => handleStatusChange(row, newStatus)}
+          />
         )
-      ) {
-        dispatch(deleteWorkFlow(id));
-      }
+      },
+      columnWidth: "170px",
+      textAlign: "center",
     },
-    [dispatch]
-  );
+    {
+      name: (
+        <Text className="zaplane-label">
+          {__("Action", "zaplane")}
+        </Text>
+      ),
+      cell: (row) => (
+        <OptionMenu
+          options={[
+            {
+              label: __('Edit', 'zaplane'),
+              icon: <Icon as={FiEdit} />,
+              type: 'button',
+              onClick: () =>
+                navigate(
+                  `${route_path}admin.php?page=zaplane-workflows&action=edit&id=${row.id}`
+                ),
+            },
+            {
+              label: __('Delete', 'zaplane'),
+              suffix: 'trash',
+              icon: <Icon as={FiTrash2} />,
+              type: 'button',
+              onClick: (id) => {
+                if (window.confirm(
+                  __("Are you sure you want to permanently delete ?", "zaplane")
+                )) {
+                  dispatch(deleteWorkFlow(row.id));
+                }
+              },
 
-  const handleStatusChange = useCallback(
-    async (item, status) => {
-      if (!item?.id || !status) return;
+              hasBorder: false,
+            },
+          ]}
 
-      await dispatch(
-        updateWorkFlowStatus({
-          status,
-          id: item.id,
-        })
-      );
+        />
+      ),
+      columnWidth: "90px",
+      textAlign: "center",
     },
-    [dispatch]
-  );
-  const columns = useMemo(
-    () => [
-      {
-        name: (
-          <Text className="zaplane-label">
-            {__("Title", "zaplane")}
-          </Text>
-        ),
-        cell: (row) => (
-          <Text
-            className="zaplane-label"
-            fontWeight="500"
-            cursor="pointer"
-            onClick={() =>
-              navigate(
-                `${route_path}admin.php?page=zaplane-workflows&action=edit&id=${row.id}`
-              )
-            }
-          >
-            {row.title}
-          </Text>
-        ),
-        columnWidth: "100px",
-        textAlign:'start'
-      },
-      {
-        name: (
-          <Text className="zaplane-label">
-            {__("Created At", "zaplane")}
-          </Text>
-        ),
-        cell: (row) => (
-          <Text fontSize="sm">{row.created_at}</Text>
-        ),
-        columnWidth: "160px",
-         textAlign: "center",
-      },
-      {
-        name: (
-          <Text className="zaplane-label">
-            {__("Updated At", "zaplane")}
-          </Text>
-        ),
-        cell: (row) => (
-          <Text className="zaplane-label">{row.updated_at}</Text>
-        ),
-        columnWidth: "160px",
-      },
-      {
-        name: (
-          <Text className="zaplane-label">
-            {__("Status", "zaplane")}
-          </Text>
-        ),
-        cell: (row) => (
-          <Box w="140px" margin="0 auto">
-            <Select
-             className="zaplane-select"
-              options={statusOptions}
-              value={statusOptions.find(
-                (opt) => opt.value === row.status
-              )}
-              onChange={(selected) =>
-                handleStatusChange(row, selected?.value)
-              }
-              isClearable={false}
-              menuPortalTarget={document.body}
-              menuPosition="fixed"
-              styles={{
-                menuPortal: (base) => ({
-                  ...base,
-                  zIndex: 9999,
-                }),
-              }}
-            />
-          </Box>
-        ),
-        columnWidth: "170px",
-         textAlign: "center",
-      },
-      {
-        name: (
-          <Text className="zaplane-label">
-            {__("Action", "zaplane")}
-          </Text>
-        ),
-        cell: (row) => (
-         <OptionMenu
-              options={[
-                {
-                  label: __('Edit', 'zaplane'),
-                  icon: <Icon as={FiEdit} />,
-                  type: 'button',
-                  onClick: () =>
-                    navigate(
-                      `${route_path}admin.php?page=zaplane-workflows&action=edit&id=${row.id}`
-                    ),
-                },
-                {
-                  label: __('Delete', 'zaplane'),
-                  suffix: 'trash',
-                  icon: <Icon as={FiTrash2} />,
-                  type: 'button',
-                  onClick: () => handleDelete(row.id),
-                  hasBorder: false,
-                },
-              ]}
+  ]
 
-            />
-        ),
-        columnWidth: "90px",
-        textAlign: "center",
-      },
-    ],
-    [navigate, handleDelete, handleStatusChange]
-  );
 
   return (
     <ListTable
