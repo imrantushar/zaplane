25e421034b57ad7fdf90bf0160255c8058f482b4
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-02-18 10:20:40 +0600

    solved global issues

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'feb23fe2b3abe6f6db9d');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'bb2bbaeeee4233f76061');
