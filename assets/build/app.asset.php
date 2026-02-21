b98b2c0b83754327eebf3b14060a12760325c832
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-02-21 20:36:54 +0600

    Add react-datepicker styles and ZAPDatePicker

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '41989fcf4b5170b8f59a');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5954097759d67b2e6329');
