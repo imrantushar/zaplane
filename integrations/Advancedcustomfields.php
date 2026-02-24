7a8eb4937259de6796c35ca7e191116bfd6deaf6
MD Mostofa Kamal Mostak <mostofa.bd7997@gmail.com>
2026-02-24 11:23:22 +0600

    Advanced-Custom-Fields-added

---
+++ integrations/Advancedcustomfields.php
@@ -0,0 +1,42 @@
+<?php
+namespace Zaplane\Integrations;
+
+use Zaplane\Framework\Classes\IntegrationBase;
+
+if (!defined('ABSPATH')) exit;
+
+class Advancedcustomfields extends IntegrationBase {
+
+    public static function get_slug(): string { return 'Advancedcustomfields'; }
+
+    public static function get_triggers(): array {
+        return [
+            'metform_after_store_form_data' => ['label'=>'Form Submitted','hook'=>'metform_after_store_form_data'],
+        ];
+    }
+
+
+       public static function resolve_trigger(array $node, array $args) {
+        
+        switch ($node['event']) {
+         case 'metform_after_store_form_data':
+          $form_data  = $args[1] ?? '';
+          if(!$form_data){
+            return [] ;
+          }
+          $result  = [];
+          foreach($form_data as $form_field  => $value){
+            $result[$form_field]  = $value ;
+          }
+          return $result ;
+
+        }
+
+        return false;
+    }
+
+    public static function execute_node(array $node, array $input): array {
+        return ['port' => 'main', 'data' => $input];
+    }
+}
+
