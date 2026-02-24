b35bcf35c09855b5ae469c9471beef4849c7170c
Jakiur <243452307+thee-prime@users.noreply.github.com>
2026-02-25 00:33:24 +0600

    Revert "register beaver builder"

--- includes/framework/core/integration-registry.php
+++ includes/framework/core/integration-registry.php
@@ -15,6 +15,4 @@ return [
     'variable' => ['file' => 'variable.php', 'class' => \Zaplane\Integrations\Variable::class],
     'webhook' => ['file' => 'webhook.php', 'class' => \Zaplane\Integrations\Webhook::class],
     'storeengine' => ['file' => 'storeengine.php', 'class' => \Zaplane\Integrations\Storeengine::class],
-    'beaverbuilder' => ['file' => 'beaverbuilder.php', 'class' => \Zaplane\Integrations\Beaverbuilder::class],
-
 ];
