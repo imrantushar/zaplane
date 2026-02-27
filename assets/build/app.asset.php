ad324a5ef42e7ca677fc4ac57f9db980055c2d40
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-02-27 20:19:05 +0600

    npm run build

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6d7c07a5cca2eaf0c925');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'eedabdfa9c78e4dee8e5');
