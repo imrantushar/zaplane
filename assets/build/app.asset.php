271bab4b14161f8cf9373ffeeb209dea5a3f36a4
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-02-28 15:38:49 +0600

    solved test deatils issues

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'eedabdfa9c78e4dee8e5');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e9d2a2be7c33410f3663');
