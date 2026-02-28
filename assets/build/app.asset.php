68cab0fcb589c1b0b4d842394594e91ef4880d19
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-02-28 23:06:52 +0600

    Comment out min-height in class library
    Commented out the `min-height: 100vh;` rule in assets/scss/common/_classLibrary.scss. This prevents the container from being forced to the full viewport height, which was affecting page layout/scrolling; the change is reversible if the full-height behavior is required.

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f01248f4fac9d436277a');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7c842fcf1b3692dcc694');
