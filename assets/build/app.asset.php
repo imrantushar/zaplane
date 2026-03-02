fd4e05ccfb689d75312f11f434bb792552508186
Sabuj Sheikh <67016132+SABUJ123-SHEIKH@users.noreply.github.com>
2026-03-02 15:11:54 +0600

    update

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1cba8aec3830424ef8a4');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '530b7a6a899ab422b63e');
