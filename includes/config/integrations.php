da2374d65dc0335d9eb9d1556666c202d73e9044
PixelnRoot <70786208+pixelnroot@users.noreply.github.com>
2026-03-03 10:19:04 +0600

    Added to the registry

--- includes/config/integrations.php
+++ includes/config/integrations.php
@@ -28,6 +28,7 @@ return [
         'metform'              => [ 'file' => 'metform.php',              'class' => \Zaplane\Integrations\Metform::class ],
         'easydigitaldownload'  => [ 'file' => 'easy-digital-download.php','class' => \Zaplane\Integrations\EasyDigitalDownload::class ],
         'academy'              => ['file' => 'academy.php',                'class' => \Zaplane\Integrations\Academy::class],
-        'bricks'              => ['file' => 'bricks.php',                'class' => \Zaplane\Integrations\Bricks::class],
+        'bricks'               => ['file' => 'bricks.php',                 'class' => \Zaplane\Integrations\Bricks::class],
+        'elementor'            => ['file' => 'elementor.php',              'class' => \Zaplane\Integrations\Elementor::class],
     ],
 ];
