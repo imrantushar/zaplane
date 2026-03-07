a3b473e4021602c8a658e4485449f68c0c0521c9
PixelnRoot <70786208+pixelnroot@users.noreply.github.com>
2026-03-08 02:34:21 +0600

    integration build

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '4b58a290a5c7355a92d1');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ce250ffd8cf832ae66e4');
