b979d50b502f07dbd7814e397c90a75ffc195dde
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-03-09 20:20:39 +0600

    add varable color

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'aadc225a7d0426570747');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '31bb627e4512a9707447');
