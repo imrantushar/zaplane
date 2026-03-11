afd181ef043410dca9de17a5412ff76d4a6ee3fa
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-03-11 15:13:22 +0600

    search icon issues solved

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '06e81128d9190f0bcf7b');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f06616a9232b7125eba8');
