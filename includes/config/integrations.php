8c4f9682c2c630a7b9792da7def2979546c19889
Rimon <mdrofiqulislam01516@gmail.com>
2026-03-30 12:37:20 +0600

    trigger add

--- includes/config/integrations.php
+++ includes/config/integrations.php
@@ -156,5 +156,9 @@ return [
 			'file' => 'academy.php',
 			'class' => \Zaplane\Integrations\Academy::class
 		],
+		'wpuserfrontend'            => [
+			'file' => 'wpuserfrontend.php',
+			'class' => \Zaplane\Integrations\Wpuserfrontend::class
+		],
 	],
 ];
