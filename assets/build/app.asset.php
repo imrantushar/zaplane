280adc4a39d562048e3abe55e25e6c582ca8daf3
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-04-06 19:55:09 +0600

    added doc

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'bbd3516d6924c300761e');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '338749ddb9ad93c481b0');
