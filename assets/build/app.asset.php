3dd4ac27adc5988aa5fe6af23feaba3db3c6e711
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-04-11 22:53:31 +0600

    update table component

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5599ad05d416a8676500');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f2bc316eb76616836f1b');
