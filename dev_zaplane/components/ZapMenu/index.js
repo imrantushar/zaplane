fba1a7452aabb3856e1b7b394e02d0931fc52197
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-04-12 11:12:50 +0600

    menu update

--- dev_zaplane/components/ZapMenu/index.js
+++ dev_zaplane/components/ZapMenu/index.js
@@ -1,20 +1,25 @@
 import React from "react";
-import { HStack, Icon, IconButton, Menu, Text } from "@chakra-ui/react"
+import { HStack, Icon, IconButton, Menu, Text } from "@chakra-ui/react";
 import { Button, Portal } from "@chakra-ui/react";
 import { __, sprintf } from "@wordpress/i18n";
 import { BsThreeDotsVertical } from "react-icons/bs";
 
-
-const ZAPMenu = ({ triggerLabel, items = [], isIcon = false, variant = "outline" }) => {
+const ZAPMenu = ({
+  triggerLabel,
+  items = [],
+  isIcon = false,
+  variant = "outline",
+  trigger,
+}) => {
   return (
     <Menu.Root>
       <Menu.Trigger asChild>
-        {isIcon ? (
-          <IconButton
-            size="sm"
-            variant={variant}
-            aria-label="More options"
-          >
+        {trigger ? (
+          trigger
+        ) : isIcon ? (
+          <IconButton onClick={e=>{
+             e.stopPropagation();
+          }} size="sm" variant={variant} aria-label="More options">
             <BsThreeDotsVertical />
           </IconButton>
         ) : (
@@ -27,23 +32,34 @@ const ZAPMenu = ({ triggerLabel, items = [], isIcon = false, variant = "outline"
       <Portal>
         <Menu.Positioner>
           <Menu.Content>
-            {items.map((item, index) => (
-              <Menu.Item
-                key={index}
-                onClick={item.onClick}
-                cursor="pointer"
-                 _hover={{
-                  bg: "var(--zaplane-gray)",
-                }}
-              >
-                 <HStack gap={2}>
-                  {item.icon && <Icon as={item.icon} boxSize={4} />}
-                  <Text m='0'>
-                    {sprintf(__('%s', 'zaplane'), item.label)}
-                  </Text>
-                </HStack>
-              </Menu.Item>
-            ))}
+            {items.map((item, index) => {
+              if (item.type === "divider") {
+                return <Menu.Separator key={index} />;
+              }
+
+              return (
+                <Menu.Item
+                  key={index}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    item.onClick(e);
+                  }}
+                  cursor="pointer"
+                  _hover={{ bg: "var(--zaplane-gray)" }}
+                >
+                  <HStack justify="space-between" w="full">
+                    <HStack gap={2}>
+                      {item.icon && <Icon as={item.icon} boxSize={4} />}
+                      <Text m="0">
+                        {sprintf(__("%s", "zaplane"), item.label)}
+                      </Text>
+                    </HStack>
+
+                    {item.rightIcon && <Icon as={item.rightIcon} />}
+                  </HStack>
+                </Menu.Item>
+              );
+            })}
           </Menu.Content>
         </Menu.Positioner>
       </Portal>
