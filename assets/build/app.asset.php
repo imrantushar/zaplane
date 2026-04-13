0d0b65235aa903656a88a6fc60a0d22b383c6a15
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-04-13 15:43:06 +0600

    reset issue solved

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '23785b06f93e143a3c13');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '08dd6aa1cddb0ed726f8');
