cafb0803d0addf205c0896166853a07aef479ba7
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-04-13 15:04:09 +0600

    topbar solved and darkmode off

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9c61dc69cea776bba3a5');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '87ca3584ace19d6a0a3f');
