44db2947de5bfcb0bf29e3652ed8f29949521d64
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-04-15 11:15:20 +0600

    icon added

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '877451faa0e26f18bb4a');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '45905f2effcc2e37a5d6');
