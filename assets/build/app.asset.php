5e8b9552e9748cac2fd839651eee1a6acadea667
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-04-15 15:54:00 +0600

    update drowar close icon margin left

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6f56d727731bfbd94238');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b53c125414fdbff78e80');
