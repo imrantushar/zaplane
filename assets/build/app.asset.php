6f21cffa0f51921a0947b3d9897f1f01ddf76d66
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-04-16 10:57:38 +0600

    solved layout zoom size issuess

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'cbad58e56cb62b67ab97');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '94da8d985b5c844b48b2');
