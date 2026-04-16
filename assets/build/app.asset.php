cc971f7e11e2e8d159ec8c7e746ad572780dfd85
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-04-16 13:31:30 +0600

    solved dainamic filed issues courser

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'dbf028710153bcbb340e');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '01bcc48cbfdedd90b419');
