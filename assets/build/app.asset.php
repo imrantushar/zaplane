8ec7419a67a63d4b4bd5336185f1e6dd47eb0036
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-04-16 10:37:08 +0600

    updateWorkFlowTitle update issues

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8d39e78d8b6a810749ff');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'cbad58e56cb62b67ab97');
