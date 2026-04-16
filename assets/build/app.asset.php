5c9deeda80b3b1c68eee649c07575fcb473b0d33
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-04-16 16:02:23 +0600

    fixed @ press issue

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7f2332cfa0e380f9dbd3');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6fb11c11c6629ae9cee9');
