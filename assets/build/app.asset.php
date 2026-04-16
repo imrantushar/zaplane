d3497d44927d69f5d2e4bbe51fcb79df502e4306
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-04-16 12:51:55 +0600

    Fix workflow canvas drawer bugs and add search to connections drawer

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b8d8519a2113b91c7166');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'dbf028710153bcbb340e');
