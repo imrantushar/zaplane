01f1e41ef06ccd5d713374ed87ea4f0fca5f684e
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-04-16 12:36:03 +0600

    solved popaver issues

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '17c0cce3f9f97337c9bb');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b8d8519a2113b91c7166');
