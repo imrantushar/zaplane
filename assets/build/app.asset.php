cb90d2697c67aab4440d22819576bdc94fc9d713
Tanjir Hasan <121637939+Tanjir-Hasan@users.noreply.github.com>
2026-04-16 17:34:50 +0600

    icons updated

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f9860baf6a0414c9a8b0');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b20dc1002e9cf5867d67');
