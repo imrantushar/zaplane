4e1577be4116da534fb1ef06bd188ece0e347f87
Tanjir Hasan <121637939+Tanjir-Hasan@users.noreply.github.com>
2026-04-19 11:31:57 +0600

    chakra scope update

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7fc6207edbdae0ce239c');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '264d85f3f5af48469f19');
--- dev_zaplane/app.js
+++ dev_zaplane/app.js
@@ -1,7 +1,6 @@
 import { createPortal } from '@wordpress/element';
 import { createRoot } from 'react-dom/client';
-import { ChakraProvider, createSystem, defaultConfig } from '@chakra-ui/react';
-import '../assets/scss/backend.scss';
+import { ChakraProvider } from '@chakra-ui/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import BackendDashboard from './containers/BackendDashboard';
@@ -10,16 +9,23 @@ import { theme } from '../assets/scss/chakra/theme';
 import { store } from '@ZAPRedux/store';
 import { WorkFlowHook } from '@ZAPContainers/BackendDashboard/pages/workflows/WorkFlowHook';
 
+import '../assets/scss/backend.scss';
+
 document.addEventListener('DOMContentLoaded', () => {
 	WorkFlowHook();
 	const container = document.getElementById('zaplane-app');
 	if (container) {
+		container.classList.add('zaplane-chakra-scope');
+
 		const root = createRoot(container);
 		const menuPage = document.getElementById('toplevel_page_zaplane');
+
 		function MenuPortal({ children }) {
 			menuPage.innerHTML = '';
+			menuPage.classList.add('zaplane-chakra-scope');
 			return createPortal(children, menuPage);
 		}
+
 		root.render(
 			<Provider store={store}>
 				<ChakraProvider value={theme}>
