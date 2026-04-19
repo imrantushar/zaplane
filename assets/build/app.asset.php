3af7bd22e831a84d1d16e45dd76423e653c8c97b
Tanjir Hasan <121637939+Tanjir-Hasan@users.noreply.github.com>
2026-04-19 13:55:18 +0600

    Production build

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd8a9c9b4dc04b2947ac2');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5c719b13df53ea2ce971');
