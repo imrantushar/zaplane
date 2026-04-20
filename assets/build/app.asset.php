33cee728715fa72f6de7b6d59566af925e158769
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-04-20 10:53:11 +0600

    build push

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6cea32a7134709d6fbe3');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'feb0d9bb918228a1d1d8');
