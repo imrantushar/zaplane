40f62b92a37b556463d3112de59d41cd751b85f4
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-04-22 09:53:56 +0600

    update sceletion

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'bd398421a75434eadd8c');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f724f13db081e39e625d');
