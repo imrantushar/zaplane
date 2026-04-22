d59e2832485f72cd58a365cb7a96d06e38c848cb
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-04-22 11:22:05 +0600

    solved global config issues

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1245e26d040c3cefb537');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ba980b25aa1ec23966bb');
