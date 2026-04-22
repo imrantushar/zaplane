e181cf6ee3ed727de183dabc1f5df27aee799ee4
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2026-04-22 13:25:07 +0600

    solved config

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ed829248ed43710adfc3');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '525eb160b0527eacfb33');
