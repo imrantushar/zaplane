50093e622dd589580370c03b80b7e61034d35737
MD Abu Abdullah <abdullah72308@gmail.com>
2026-05-06 18:00:08 +0600

    build(ui): update tailwind configuration and rebuild assets
    Update `tailwind.config.js` to disable preflight and set the important selector to `#zaplane-app` to prevent style conflicts. Rebuild CSS assets and update the asset version in `app.asset.php`.

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c9058ba0e58b0e151470');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9875933f8932c45247d2');
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./dev_zaplane/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+		'./dev_zaplane/**/*.{js,jsx,ts,tsx}',
+	],
+	important: '#zaplane-app',
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+	corePlugins: {
+		preflight: false,
+	},
+};
