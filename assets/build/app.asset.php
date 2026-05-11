52153f1b1b3125c5f000cbdfc797fd9d1e228c4d
MD Ali Kadar <webdevs2012@gmail.com>
2026-05-11 09:27:41 +0600

    build push

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6b1ebbf1b36894823628');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2961780932184c4429bc');
