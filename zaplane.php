9725b12b1c6aa33534d3b209a873dba89dfeba22
Tushar Imran <tusharimran9909@gmail.com>
2026-06-25 19:23:09 +0600

    Production Build

--- zaplane.php
+++ zaplane.php
@@ -3,7 +3,7 @@
  * Plugin Name:     Zaplane
  * Plugin URI:      https://zaplane.app/
  * Description:     WordPress Automation Plugin
- * Version:         1.0.1
+ * Version:         1.0.2
  * Author:          kodezen
  * Author URI:      https://kodezen.com
  * License:         GPL-3.0+
@@ -61,7 +61,7 @@ final class Zaplane {
 	}
 
 	public function define_constants(): void {
-		define( 'ZAPLANE_VERSION', '1.0.1' );
+		define( 'ZAPLANE_VERSION', '1.0.2' );
 		define( 'ZAPLANE_ALLOW_LOGS', true );
 		define( 'ZAPLANE_PLUGIN_SLUG', 'zaplane' );
 		define( 'ZAPLANE_PLUGIN_FILE', __FILE__ );
