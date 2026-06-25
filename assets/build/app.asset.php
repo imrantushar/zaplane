a192a9929dfe25b5cde1321ae9b127ee5a94d304
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-06-25 14:03:11 +0600

    Select UI issue fixed

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2bdf3b2e3fb09c923367');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'adf194faa946210f4bcf');
