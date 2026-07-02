7b4b5b60b36b8c2b120e7e6b8484f99cefc7f535
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-07-02 15:05:19 +0600

    Folder rename ui fixed

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '4e86004ccbf2d5b910b9');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3988f26b6452a8333895');
