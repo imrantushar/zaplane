e4ce6ba0b39b1141c59af6bbc36bae714b3814f7
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-07-02 14:40:32 +0600

    action menu position issue fixed

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b72e20b5a079c3024408');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'df44ec22c5f41efc3a31');
