ff71f7cc684cfaef26b287bf545b95f4dbb1303d
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-07-02 14:44:07 +0600

    Label issue fixed

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'df44ec22c5f41efc3a31');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '4e86004ccbf2d5b910b9');
--- dev_zaplane/containers/BackendDashboard/pages/workflows/workFlowMotion/actionDrawer/ActionDrawer.js
+++ dev_zaplane/containers/BackendDashboard/pages/workflows/workFlowMotion/actionDrawer/ActionDrawer.js
@@ -162,7 +162,7 @@ const {
       setFieldValue("actionType", "");
     }}
     // closeOnOverlayClick
-    title={!mode ? "Add Action" : selectedItem?.name || __('App', 'zaplane')} placement="end"
+    title={!mode ? (isTrigger ? __('Add Trigger', 'zaplane') : __('Add Action', 'zaplane')) : selectedItem?.name || __('App', 'zaplane')} placement="end"
     // size={["filter", "condition"].includes(values?.actionType) ? "xl" : "md"}
     footer={<div className="flex items-center justify-end gap-3">
       <button
