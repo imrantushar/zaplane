4962bf508c8bd84e092f09c1f75c6385433efaa4
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-07-07 13:05:08 +0600

    Field UI issue fixed and icon fallback added

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'bde3c491e88f2bc927f1');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '62e869c0d53ab3a1139f');
