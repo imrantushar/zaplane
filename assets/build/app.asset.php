df832831ee8e20f014e39cb0055e9ed95389b2a3
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-07-07 12:25:21 +0600

    checkbox ui issue fixed

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd894fc26d53244373cdf');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'bde3c491e88f2bc927f1');
