00ec4eda16877626659bb84615f2ef5af02db81e
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-07-14 11:47:32 +0600

    Email template card UI updated

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '075d6cbbf4b1fefb901c');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'af0dd8a36b2261947c5f');
