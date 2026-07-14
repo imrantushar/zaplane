928ce9bbea1dcc046919cb266842957da1b6de24
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-07-14 14:17:22 +0600

    tab ui updated

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c0ddf93cf1df3aaeb75c');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'fa77343c9511afc0e0d3');
