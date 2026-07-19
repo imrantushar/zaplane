6a16db912788c08b91193d5783abc3f3d7b0652d
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-07-19 16:24:03 +0600

    Unnecessary css removed

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6617a8ff8fa23c88d1a7');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '481acec35748f7eb892c');
