3c083a985434b4a60901d7cf037d524c8675be1e
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-08-27 17:46:26 +0600

    custom app checkbox issue fixed

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'cd08364cd53f0789e5c4');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ffe134803b984960ec2a');
