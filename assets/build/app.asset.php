9bafde51c15335078a3d03329efe412d57a4ea22
Fahim Hasan <43146740+fahim-tonmoy@users.noreply.github.com>
2026-08-27 17:37:39 +0600

    checkbox issue fixed

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c4cb07699b5a13f2b0fc');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'cd08364cd53f0789e5c4');
