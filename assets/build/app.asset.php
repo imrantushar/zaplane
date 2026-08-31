bd6a247a02b94f0959cca273b27caa3088af8a17
Shahariar <149128436+shahariar270@users.noreply.github.com>
2026-08-31 18:40:57 +0600

    kodezen editor issue sloved

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ffe134803b984960ec2a');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '36d223d4efa72257ba14');
