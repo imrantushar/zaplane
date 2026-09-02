1e9c8b1c0448a80aa6134a2958470c6b54df02c5
Shahariar <149128436+shahariar270@users.noreply.github.com>
2026-09-02 16:49:35 +0600

    field asterisk  implemented

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ffe134803b984960ec2a');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a1bfb6ca6f53a4b8d9a9');
