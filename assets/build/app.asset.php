dc75b4b9ad04db49b3dad3b6c3cb152a81a5f9da
Shahariar <149128436+shahariar270@users.noreply.github.com>
2026-09-08 17:16:54 +0600

    workflow update button UI updated

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '03b5e7c84ea03bf73334');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd0b12407828526bc393c');
