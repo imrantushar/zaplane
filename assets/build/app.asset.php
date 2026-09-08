9c7e4aad19fbec9a86cb87bfffe6d4328436c1de
Shahariar <149128436+shahariar270@users.noreply.github.com>
2026-09-08 16:02:39 +0600

    pakage updated and UI look change

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ffe134803b984960ec2a');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '03b5e7c84ea03bf73334');
