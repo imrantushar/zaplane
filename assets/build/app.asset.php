ade9d634433a2c9f55b259f313c611c61a46e53d
Shahariar <149128436+shahariar270@users.noreply.github.com>
2026-09-08 17:47:43 +0600

    build push

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd0b12407828526bc393c');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a6f2ba3827f85e29f4f6');
