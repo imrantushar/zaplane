5884f9af90e6ef2a413a0f41e30169aa55f886b9
Md Rofiqul  Islam Rimon <mdrofiqulislam01516@gmail.com>
2026-09-09 15:37:35 +0600

    ui issu fix

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'fe3d8b7f9ddf4520478e');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3fe3b93b5b9627d8de37');
