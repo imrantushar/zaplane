726700720525c287ce6b3f8137637868a449b718
Shahariar <149128436+shahariar270@users.noreply.github.com>
2026-09-13 16:49:14 +0600

    add copy button on

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5e6dff2a03c09ce040cb');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'be990258526dd77a577a');
