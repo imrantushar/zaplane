b25133f6caf66b2c228f2fd41752f13cae747d9c
Mustafizur Rahman Mahin <mahin.wpdev@gmail.com>
2026-09-20 12:54:28 +0600

    build push

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e64e7371608a106a3c94');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8cec448325525eef538f');
