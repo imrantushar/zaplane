2fd4ac2f16f7e7a1fd7e7101eeb4cbc17cb0e172
Tushar Imran <tusharimran9909@gmail.com>
2026-09-24 16:01:44 +0600

    Production Build

--- assets/build/app.asset.php
+++ assets/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '555f90e602a381188197');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0627f7e2aa5cc53ec8b7');
